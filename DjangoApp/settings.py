3ea9b80f0e0fe92941a26c74b9bba41686c968c2
TopherF22 <cfronda@fordham.edu>
2023-12-13 22:38:25 -0500

    Updated Allowed Hosts

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'TophersDashboard.azurewebsites.net']
 
 # Application definition
 

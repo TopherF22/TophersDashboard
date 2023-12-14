b80b90935333de1057894e2f0f1c5d0835f2f076
TopherF22 <cfronda@fordham.edu>
2023-12-14 18:43:19 -0500

    Stage 5- acceptance

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True  
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = ['https://tophersdashboard.azurewebsites.net']
+
 ROOT_URLCONF = 'DjangoApp.urls'
 
 TEMPLATES = [
